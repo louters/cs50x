a54d3235de59f4185beab6ebc9c2c9b467d3adc3
Leandre Outers <outers.leandre@gmail.com>
2020-04-20 14:05:55 +0100

    Added function count_words

--- readability.c
+++ readability.c
@@ -4,15 +4,24 @@
 #include <ctype.h>
 
 int count_letters(string text);
+int count_words(string text);
 
 int main(void)
 {
+    //Prompt user for input text.
     string text = get_string("Text: ");
+
     int letters = count_letters(text);
+    printf("%i letter(s)\n", letters);
+
+    int words = count_words(text);
+    printf("%i word(s)\n", words);
+
     return 0;
 }
 
 int count_letters(string text)
+// Returns nbr of letters in string <text>.
 {
     int cnt = 0;
     for (int i=0, n = strlen(text); i < n ; i++)
@@ -22,5 +31,20 @@ int count_letters(string text)
             cnt++;
         }
     }
+    return cnt;
+}
+
+int count_words(string text)
+// Return nbr of words in <text>.
+{
+    int cnt = 0;
+    for (int i=0, n = strlen(text); i < n ; i++)
+    {
+        if (isblank(text[i]))
+        {
+            cnt++;
+        }
+    }
+    cnt++;
     return cnt;
 }
