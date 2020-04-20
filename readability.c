e7c3c70e49fd2b7c8cf024364fdc91ffdfa4463b
Leandre Outers <outers.leandre@gmail.com>
2020-04-20 14:22:45 +0100

    Add function count_sentences

--- readability.c
+++ readability.c
@@ -5,17 +5,21 @@
 
 int count_letters(string text);
 int count_words(string text);
+int count_sentences(string text);
 
 int main(void)
 {
     //Prompt user for input text.
     string text = get_string("Text: ");
 
-    int letters = count_letters(text);
-    printf("%i letter(s)\n", letters);
+    int l = count_letters(text);
+    printf("%i letter(s)\n", l);
 
-    int words = count_words(text);
-    printf("%i word(s)\n", words);
+    int w = count_words(text);
+    printf("%i word(s)\n", w);
+
+    int s = count_sentences(text);
+    printf("%i sentence(s)\n", s);
 
     return 0;
 }
@@ -46,5 +50,19 @@ int count_words(string text)
         }
     }
     cnt++;
+    return cnt;
+}
+
+int count_sentences(string text)
+// Return nbr of sentences in <text>.
+{
+    int cnt = 0;
+    for (int i=0, n = strlen(text); i < n ; i++)
+    {
+        if (text[i] == '!' || text[i] == '.' || text[i] == '?')
+        {
+            cnt++;
+        }
+    }
     return cnt;
 }
