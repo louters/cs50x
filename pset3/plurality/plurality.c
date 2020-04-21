f764eaa1d66f3dfa3a13e17e29601f8545add268
Leandre Outers <outers.leandre@gmail.com>
2020-04-21 11:27:18 +0100

    Filled & tested vote funciton

--- pset3/plurality/plurality.c
+++ pset3/plurality/plurality.c
@@ -66,7 +66,14 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(name, candidates[i].name) == 0)
+        {
+            candidates[i].votes++;
+            return true;
+        }
+    }
     return false;
 }
 
