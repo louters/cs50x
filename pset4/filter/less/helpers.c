b196efbbfa34972c8aa578ba5a7d49128e2d5ec3
Leandre Outers <outers.leandre@gmail.com>
2020-04-22 16:19:10 +0100

    grayscale function - does not work on local but on ide

--- pset4/filter/less/helpers.c
+++ pset4/filter/less/helpers.c
@@ -1,8 +1,23 @@
+#include <math.h>
+
 #include "helpers.h"
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    int mean = -1;
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            mean = image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed;
+            mean = round( (double) mean / 3); 
+
+            image[i][j].rgbtBlue = (BYTE) mean;
+            image[i][j].rgbtGreen = (BYTE) mean;
+            image[i][j].rgbtRed = (BYTE) mean;
+        }
+    }
     return;
 }
 
