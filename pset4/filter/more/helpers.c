ac6224894e14b846ecce3a8dd6617936854cf754
Leandre Outers <outers.leandre@gmail.com>
2020-04-23 17:16:29 +0100

    Initial commit: pset4-filter-more

---
+++ pset4/filter/more/helpers.c
@@ -0,0 +1,131 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#include "helpers.h"
+
+// Blur a pixel in an image
+void blur_pixel(int height, int width, RGBTRIPLE image[height][width],
+                RGBTRIPLE image_copy[height][width], int row, int col);
+
+// Swap two RGBTRIPLE
+void swap(RGBTRIPLE *a, RGBTRIPLE *b);
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    int mean = -1;
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            mean = image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed;
+            mean = round((double) mean / 3);
+
+            image[i][j].rgbtBlue =  mean;
+            image[i][j].rgbtGreen =  mean;
+            image[i][j].rgbtRed =  mean;
+
+        }
+    }
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < ((double) width - 1) / 2; j++)
+        {
+            // Swap both ends of row i
+            swap(&image[i][j], &image[i][width - 1 - j]);
+        }
+    }
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    // Copy image
+    RGBTRIPLE(*image_copy)[width] = calloc(height, width * sizeof(RGBTRIPLE));
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image_copy[i][j] = image[i][j];
+        }
+    }
+
+    // Blur each pixel
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            blur_pixel(height, width, image, image_copy, i, j);
+        }
+    }
+
+    // Free memory from image copy
+    free(image_copy);
+
+    return;
+}
+
+// Detect edges
+void edges(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Blur pixel in an image.
+void blur_pixel(int height, int width, RGBTRIPLE image[height][width],
+                RGBTRIPLE image_copy[height][width], int row, int col)
+{
+    int neighbors = 0; // Neighbor count, includes pixel itself
+    int mean_red = 0;
+    int mean_green = 0;
+    int mean_blue = 0;
+
+    for (int i = -1; i < 2; i++)
+    {
+        // Manage edge & corners
+        if (row + i >= 0 && row + i < height)
+        {
+            for (int j = -1; j < 2; j++)
+            {
+                // Manage edge & corners
+                if (col + j >= 0 && col + j < width)
+                {
+                    neighbors++; // Increase neighbour count
+
+                    // Sum colors of each pixel in the box
+                    mean_red += image_copy[row + i][col + j].rgbtRed;
+                    mean_green += image_copy[row + i][col + j].rgbtGreen;
+                    mean_blue += image_copy[row + i][col + j].rgbtBlue;
+                }
+            }
+        }
+    }
+    // Get rounded int mean per color
+    mean_red = round((double) mean_red / neighbors);
+    mean_green = round((double) mean_green / neighbors);
+    mean_blue = round((double) mean_blue / neighbors);
+
+    // Assign mean colors to original image
+    image[row][col].rgbtRed = mean_red;
+    image[row][col].rgbtGreen = mean_green;
+    image[row][col].rgbtBlue = mean_blue;
+
+}
+
+// Swap two RGBTRIPLE
+void swap(RGBTRIPLE *a, RGBTRIPLE *b)
+{
+    RGBTRIPLE tmp;
+    tmp = *a;
+    *a = *b;
+    *b = tmp;
+    return;
+}
