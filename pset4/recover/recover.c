8cf70172347e5de1f2ee6189123a17f0ada1222c
Leandre Outers <outers.leandre@gmail.com>
2020-04-27 12:28:34 +0100

    Add jpeg name mechanism & file read until end

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -1,7 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+// Check for jpeg signature
+bool is_jpeg(unsigned char buffer[]);
 
 int main(int argc, char *argv[])
 {
+    // Ensure proper usage
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: ./recover image\n");
+        return 1;
+    }
 
+    // Remember filename
+    char *filename = argv[1];
+
+    // Open input file
+    FILE *file = fopen(filename, "rb");
+    if (file == NULL)
+    {
+        fprintf(stderr, "Could not open %s\n", filename);
+        return 1;
+    }
+
+    // JPEG counter
+    int cnt = 0;
+    char *jpeg_name = malloc(sizeof("###.jpg"));
+
+    // Buffer of 512B. Allocate memory of buffer.
+    unsigned char buffer [512];
+
+    // Read input file until end
+    while (fread(buffer, sizeof(buffer), 1, file))
+    {
+        // Test whether start of a jpeg
+        if( is_jpeg(buffer))
+        {
+            // Create JPEG output filename
+            sprintf(jpeg_name, "%03i.jpg", cnt);
+
+            // Open JPEG output file
+            FILE *outfile = fopen(jpeg_name, "w");
+            printf("%s\n", jpeg_name);
+            // // while no new signature
+            // do             
+            // {
+            //     // Write to JPEG
+            //     //printf("%s\n", buffer);
+            //     fprintf(outfile, "%s", buffer);
+            //     // Read from file
+            //     fread(buffer, sizeof(buffer), 1, file);
+            // }
+            // while (!is_jpeg(buffer));
+
+
+            // Close JPEG output file
+            fclose(outfile);
+            cnt++;
+        }
+    }
+
+    // Close input file
+    fclose(file);
+
+    // Free memory
+    free(jpeg_name);
+
+    return 0;
+}
+
+// Check for JPEG signature
+bool is_jpeg(unsigned char buffer[])
+{
+    if (
+        buffer[0] == 0xff
+        && buffer[1] == 0xd8
+        && buffer[2] == 0xff
+        && (buffer [4] >> 4) ^ 0b1110
+        )
+        {
+            return true;
+        }
+    return false;
 }
