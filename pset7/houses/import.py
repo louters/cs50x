e1d0bfb5183e2cd8528a7cd8a623efec4d7ffaa1
Leandre Outers <outers.leandre@gmail.com>
2020-05-05 17:26:22 +0100

    Add submitted pset7-houses

--- pset7/houses/import.py
+++ pset7/houses/import.py
@@ -1,1 +1,53 @@
-# TODO
+from sys import argv
+import csv
+import sqlite3
+
+
+def main():
+    # Control command prompt 
+    if len(argv) != 2:
+        print("Usage: python import.py <csv file>")
+        exit(1)
+
+    # Create empty db
+    open(f"students.db", "w").close()
+
+    # Connect DB & create cursor
+    conn = sqlite3.connect("students.db")
+    c = conn.cursor()
+
+    # Create students table
+    c.execute("CREATE TABLE students (id INTEGER PRIMARY KEY AUTOINCREMENT, first VARCHAR(255), middle VARCHAR(255), last VARCHAR(255), house VARCHAR(10), birth INTEGER)")
+    
+    # Read CSV and insert data into DB
+    with open(argv[1], 'r') as f:
+        # Create DictReader
+        reader = csv.DictReader(f)
+
+        # Iterate over CSV file
+        for row in reader:
+            # Student's full name
+            full_name = [None] * 3
+            full_name[0] = row["name"].split()[0]
+            full_name[2] = row["name"].split()[-1]
+            # Treat middle name
+            if len(row["name"].split()) == 3:
+                full_name[1] = row["name"].split()[1]
+            
+            # SQL Query
+            q = "INSERT INTO students (first, middle, last, house, birth) VALUES (?, ?, ?, ?, ?)"
+            # Parameters for SQL Query
+            p = [full_name[0], full_name[1], full_name[2], row["house"], int(row["birth"])]
+            # Execute SQL query
+            c.execute(q, p)
+
+    # Save changes
+    conn.commit()
+    # Close cursor 
+    c.close()
+    # Close connection
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
--- pset7/houses/roster.py
+++ pset7/houses/roster.py
@@ -1,1 +1,38 @@
-# TODO
+from sys import exit, argv
+import sqlite3
+
+
+def main():
+    # Check for command prompt
+    if len(argv) != 2:
+        print("Usage: python roster.py <house>")
+        exit(1)
+    
+    # House
+    house = [argv[1]]
+
+    # Connect to students DB
+    conn = sqlite3.connect("students.db")
+    c = conn.cursor()
+
+    # Query
+    q = "SELECT * FROM students WHERE house = ? ORDER BY last, first"
+    # Execute query
+    c.execute(q, house)
+    roster = c.fetchall()
+
+    # Print roster
+    for r in roster:
+        print(f"{r[1]}", end=' ')
+        if r[2]:
+            print(f"{r[2]}", end=' ')
+        print(f"{r[3]},", end=' ')
+        print(f"born {r[5]}")
+        
+    # Close db
+    c.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
